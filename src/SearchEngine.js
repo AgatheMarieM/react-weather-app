4ba7515f88d7ddaad3534d9a221b402b72c4247e
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-01-05 11:56:23 +0100

    Added h1 and h2

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 export default function SearchEngine() {
     const [city, setCity] = useState("");
+
 function handleSubmit(event){
     alert(`${city}`);
 }
@@ -9,12 +10,15 @@ function updateCity(event) {
     setCity(event.target.value);
 
 }
-
     return (<div className="SearchEngine">
         <form onSubmit={handleSubmit}>
             <input type="search" placeholder="Type a city" onChange={updateCity}/>
             <button type="submit">Go</button>
             </form>
+        <h1>{city}</h1>
+        <h2>Last updated on: date and time</h2>
+
+
             </div>) 
 
 }
