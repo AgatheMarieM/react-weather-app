d1ec3ac1ac66acc238fff209ec386cde56247a86
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-01-05 12:16:13 +0100

    Created Footer with Link to Github

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+export default function Footer() {
+    return(
+        <div className="Footer">
+            <small>
+        <a
+          href="https://github.com/AgatheMarieM/react-weather-app"
+          target="_blank"
+        >
+          Open source{" "}
+        </a>
+        code by Agathe M.
+      </small>
+        </div>
+    )
+}
--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,8 +1,10 @@
 import React from "react";
 import SearchEngine from "./SearchEngine";
+import Footer from "./Footer";
 
 export default function WeatherApp() {
     return <div className="WeatherApp">
         <SearchEngine />
+        <Footer />
         </div>
 }
