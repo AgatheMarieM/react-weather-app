26bf32153c723d86b07bdc8abaa6cdce7ba462c0
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-01-11 17:52:53 +0100

    changed dummy icon css

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactAnimatedWeather from 'react-animated-weather';
 
 export default function WeatherIcon(props) {
-    return  <ReactAnimatedWeather
+    return <div className="today-icon">
+        <ReactAnimatedWeather
     icon="CLOUDY"
     color= "#ec6e4c"
     size= {60}
     animate={true}
-  />
+     /></div> 
 }
