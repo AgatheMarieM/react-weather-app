68e593f46a791cd4eed4024e78cd0b814bc28551
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-01-11 14:06:05 +0100

    Included day, hour, minutes inside FormattedDate component

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,8 +1,15 @@
 import React from "react";
 
-export default function Date(props) {
+export default function FormattedDate(props) {
 console.log(props.date)
-    return (<div className="Date">
-        Last updated on: 
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[props.date.getDay()];
+let hour = props.date.getHours();
+hour = (hour < 10 ? `0` : ``) + hour;
+let minutes = props.date.getMinutes();
+minutes = (minutes < 10 ? `0` : ``) + minutes;
+
+return (<div className="FormattedDate">
+        Last updated on: {day}, {hour}:{minutes}
     </div>)
 }
