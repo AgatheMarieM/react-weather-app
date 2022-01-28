1c335db1f1515ffda51fac1f1ad2dafed4675f0c
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-01-28 21:43:22 +0100

    updated Footer with OpenWeather link

--- src/App.js
+++ src/App.js
@@ -14,7 +14,10 @@ function App() {
         <a href="https://github.com/AgatheMarieM/react-weather-app" target="_blank" rel="noreferrer"> 
         Open source{" "}
         </a>
-        code by Agathe M.
+        code by Agathe M. using{" "}
+        <a href="https://openweathermap.org/api" target="_blank" rel="noreferrer"> 
+        OpenWeather API{" "}
+        </a>
       </small>
     </div>
     </div>
